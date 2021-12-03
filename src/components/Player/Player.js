805b58080fbdc968a1c4b909be08fe41d74d8a68
Mark Voltaire <markjvoltaire@gmail.com>
2021-12-02 22:06:34 -0500

    selectors added

---
+++ src/components/Player/Player.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Player.css';
+
+export default function Main() {
+  return (
+    <div className="selector">
+      {' '}
+      <select className="head-picker">
+        <option value="chris-paul">Chris Paul</option>
+        <option value="lebron-james">Lebron James</option>
+        <option value="jason-kidd">Jason Kidd</option>
+      </select>
+      <select className="body-picker">
+        {' '}
+        Select A Body
+        <option value="curry-body">Steph Curry</option>
+        <option value="durant-body">Kevin Durant</option>
+        <option value="shaq-body">Shaquille Oneal</option>
+      </select>
+      <select value="shoe-picker">
+        {' '}
+        hello
+        <option value="iverson-shoes">Allen Iverson</option>
+        <option value="jordan-shoes">Micheal Jordan</option>
+        <option value="kobe-shoes">Kobe Bryant</option>
+      </select>
+    </div>
+  );
+}
